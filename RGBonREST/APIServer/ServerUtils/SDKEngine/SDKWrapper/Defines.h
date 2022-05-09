05cd36d70564173e71c33dfb35df3918df87433f
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2022-05-09 19:48:33 +0900

    Working on Defines

--- RGBonREST/APIServer/ServerUtils/SDKEngine/SDKWrapper/Defines.h
+++ RGBonREST/APIServer/ServerUtils/SDKEngine/SDKWrapper/Defines.h
@@ -8,4 +8,38 @@
 #ifndef RGBONREST_DEFINES_H
 #define RGBONREST_DEFINES_H
 
+#include <string>
+
+using namespace std;
+
+enum DeviceType{
+    Mouse = 1,
+    Headset = 2,
+    Keyboard = 3,
+    Mousemat = 4,
+    HeadsetStand = 5,
+    GPU = 6,
+    Mainboard = 7,
+    Microphone = 8,
+    Cooler = 9,
+    ETC = 10,
+    ALL = 0
+};
+
+enum Errors{
+    SDKAlreadyConnected = 1,
+    SDKNotConnected = 2,
+    NoDevicesConnected = 3,
+    SDKUnexpectedError = 4,
+    SDKServiceNotRunning = 5,
+    InvalidRGBValue = 6,
+    InvalidDeviceType = 7
+};
+
+typedef struct _device{
+    string name;
+    string sdkName;
+    DeviceType deviceType;
+} Device;
+
 #endif //RGBONREST_DEFINES_H
