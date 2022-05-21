5ed24a277f59528a8854be7f77c2f34d7c72d1c7
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2022-05-21 17:58:54 +0900

    Checked Razer and Corsair

--- RGBonREST/Tests/RazerTest.cpp
+++ RGBonREST/Tests/RazerTest.cpp
@@ -8,13 +8,13 @@
 #include "RazerTest.h"
 
 void RazerTest() {
-    RazerSDK* razerSDK = new RazerSDK();
+    RazerSDK *razerSDK = new RazerSDK();
     printf("INIT DONE\n");
     printf("Testing Connection for Razer : %d\n\n", razerSDK->connect());
     vector<Device> allDevices = razerSDK->getDevices();
 
     cout << "Connected Devices : " << endl;
-    for (auto const& x : allDevices){
+    for (auto const &x: allDevices) {
         cout << "Device Name : " << (string) x.name << endl;
         cout << "SDK Name : " << (string) x.sdkName << endl;
         cout << "Device Type : " << to_string(x.deviceType) << endl;
@@ -22,18 +22,34 @@ void RazerTest() {
 
     printf("Testing RainbowAll");
 
-    for (int i = 0 ; i < 255 ; i++)
+    for (int i = 0; i < 255; i++) {
+        Sleep(10);
         razerSDK->setRgb(DeviceType::ALL, 255, i, 0);
-    for (int i = 0 ; i < 255 ; i++)
-        razerSDK->setRgb(DeviceType::ALL, 255 - i , 255, 0);
-    for (int i = 0 ; i < 255 ; i++)
+    }
+    for (int i = 0; i < 255; i++) {
+        Sleep(10);
+
+        razerSDK->setRgb(DeviceType::ALL, 255 - i, 255, 0);
+    }
+    for (int i = 0; i < 255; i++) {
+        Sleep(10);
         razerSDK->setRgb(DeviceType::ALL, 0, 255, i);
-    for (int i = 0 ; i < 255 ; i++)
+    }
+    for (int i = 0; i < 255; i++) {
+        Sleep(10);
+
         razerSDK->setRgb(DeviceType::ALL, 0, 255 - i, 255);
-    for (int i = 0 ; i < 255 ; i++)
+    }
+    for (int i = 0; i < 255; i++) {
+        Sleep(10);
+
         razerSDK->setRgb(DeviceType::ALL, i, 0, 255);
-    for (int i = 0 ; i < 255 ; i++)
+    }
+    for (int i = 0; i < 255; i++) {
+        Sleep(10);
+
         razerSDK->setRgb(DeviceType::ALL, 255, 0, 255 - i);
+    }
 
     printf("\nDisconnecting Devices : %d\n\n", razerSDK->disconnect());
 }
--- RGBonREST/main.cpp
+++ RGBonREST/main.cpp
@@ -1,7 +1,9 @@
 #include "Tests/CorsairTest.h"
 #include "Tests/RazerTest.h"
+#include "Tests/All.h"
 
 int main(){
     //CorsairTest();
-    RazerTest();
+    //RazerTest();
+    AllTest();
 }
